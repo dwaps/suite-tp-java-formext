5cd2512685eaff0d008a8478d5c267019b5b49ea
Michael Cornillon <contact@dwaps.fr>
2017-10-22 19:44:32 +0200

    Calcul du mois via fonction : étape 2

--- HandlerDate.java
+++ HandlerDate.java
@@ -36,23 +36,36 @@ public class HandlerDate {
 	}
 	
 	private void searchMonth() {
-		int cpt31Days = 1, cpt30Days = 1;
-		
-		if (nbOfDay <= MONTH_31DAYS) month = 1;
-		
-		// Incrémentation ?
-		// cpt31Days => NON OUI NON OUI NON OUI OUI NON OUI NON OUI
-		// cpt30Days => NON NON OUI NON OUI NON NON OUI NON OUI NON
-		else if (nbOfDay <= (MONTH_31DAYS*cpt31Days + 		MONTH_30DAYS*cpt30Days-1)) month = 2;
-		else if (nbOfDay <= (MONTH_31DAYS*(++cpt31Days) + 	MONTH_30DAYS*cpt30Days-1)) month = 3;
-		else if (nbOfDay <= (MONTH_31DAYS*cpt31Days + 		MONTH_30DAYS*(++cpt30Days)-1)) month = 4;
-		else if (nbOfDay <= (MONTH_31DAYS*(++cpt31Days) + 	MONTH_30DAYS*cpt30Days-1)) month = 5;
-		else if (nbOfDay <= (MONTH_31DAYS*cpt31Days + 		MONTH_30DAYS*(++cpt30Days)-1)) month = 6;
-		else if (nbOfDay <= (MONTH_31DAYS*(++cpt31Days) + 	MONTH_30DAYS*cpt30Days-1)) month = 7;
-		else if (nbOfDay <= (MONTH_31DAYS*(++cpt31Days) + 	MONTH_30DAYS*cpt30Days-1)) month = 8;
-		else if (nbOfDay <= (MONTH_31DAYS*cpt31Days + 		MONTH_30DAYS*(++cpt30Days)-1)) month = 9;
-		else if (nbOfDay <= (MONTH_31DAYS*(++cpt31Days) + 	MONTH_30DAYS*cpt30Days-1)) month = 10;
-		else if (nbOfDay <= (MONTH_31DAYS*cpt31Days + 		MONTH_30DAYS*(++cpt30Days)-1)) month = 11;
-		else if (nbOfDay <= (MONTH_31DAYS*(++cpt31Days) + 	MONTH_30DAYS*cpt30Days-1)) month = 12;
+		int cpt31Days = 0, cpt30Days = 0;
+		final boolean[] is30DaysMonth = {
+			false, // Janvier
+			true, // Février (30-1 !!!)
+			false, // Mars
+			true, // Avril
+			false, // Mai
+			true, // Juin
+			false, // Juillet
+			false, // Août
+			true, // Septembre
+			false, // Octobre
+			true, // Novembre
+			false // Décembre
+		};
+
+		// SIMULATION DES TOURS DE BOUCLES
+		// 1er tour de boucle : test sur 31 jours seulement
+		if (nbOfDay <= MONTH_31DAYS*(is30DaysMonth[0] ? cpt31Days : ++cpt31Days)) month = 1;
+		// A partir de 3ème tour de boucle : test avec irrégularité du mois de février pris en compte
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[1] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[1] ? ++cpt30Days : cpt30Days)-1)) month = 2;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[2] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[2] ? ++cpt30Days : cpt30Days)-1)) month = 3;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[3] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[3] ? ++cpt30Days : cpt30Days)-1)) month = 4;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[4] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[4] ? ++cpt30Days : cpt30Days)-1)) month = 5;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[5] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[5] ? ++cpt30Days : cpt30Days)-1)) month = 6;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[6] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[6] ? ++cpt30Days : cpt30Days)-1)) month = 7;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[7] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[7] ? ++cpt30Days : cpt30Days)-1)) month = 8;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[8] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[8] ? ++cpt30Days : cpt30Days)-1)) month = 9;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[9] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[9] ? ++cpt30Days : cpt30Days)-1)) month = 10;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[10] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[10] ? ++cpt30Days : cpt30Days)-1)) month = 11;
+		else if (nbOfDay <= (MONTH_31DAYS*(is30DaysMonth[11] ? cpt31Days : ++cpt31Days) + 	MONTH_30DAYS*(is30DaysMonth[11] ? ++cpt30Days : cpt30Days)-1)) month = 12;
 	}
 }
